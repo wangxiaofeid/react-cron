dd2de7870f78b36cf3bea492b3981d56351b119b
wangxiaofeid <979436452@qq.com>
2020-03-16 11:48:30 +0800

    文档更新

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # react-cron
-react版本Cron表达式
+
+目前市面上的 Cron 表达式相对复杂，可是实际场景中用不到太过复杂的，故开发此组件。
+
+特点
+
+-   简洁、好实用
+-   易于理解
+
+# api
+
+| 参数         | 说明                 | 类型            | 默认值 |
+| ------------ | -------------------- | --------------- | ------ |
+| defaultValue | 指定默认值           | String          | -      |
+| value        | 指定当前 Cron 值     | String          | -      |
+| onChange     | 修改 Cron 调用此函数 | function(value) | -      |
+| multiple     | 是否可以多选         | Object          | -      |
+
+# 示例
+
+<div align="left">
+    <img width=552 src="./snapshot.png" alt="效果图" />
+</div>
+
+```jsx
+import React, { Fragment, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import Corn from './src/index.js';
+
+const ml10 = { marginLeft: 10 };
+const mt20 = { marginTop: 20 };
+
+export default function CornDemo() {
+    const [value, setValue] = useState('6 6 14 ? * 6 *');
+    const [value2, setValue2] = useState('0 0 0 1,6 * ? *');
+    const [value3, setValue3] = useState('0 0 0 L * ? *');
+    const [value4, setValue4] = useState('0 0 0 L 4/12 ? *');
+    return (
+        <Fragment>
+            <h4>普通：</h4>
+            <Corn
+                value={value}
+                onChange={value => {
+                    setValue(value);
+                }}
+            />
+            <Button
+                style={ml10}
+                onClick={() => {
+                    setValue('0 0 0 2 * ? *');
+                }}
+                type="primary"
+            >
+                重置
+            </Button>
+            {value}
+
+            <h4 style={mt20}>多选：</h4>
+            <Corn
+                value={value2}
+                onChange={value => {
+                    setValue2(value);
+                }}
+                multiple
+            />
+            <Button
+                style={ml10}
+                onClick={() => {
+                    setValue2('6 4 4 ? * 1,3,5 *');
+                }}
+                type="primary"
+            >
+                重置
+            </Button>
+            {value2}
+
+            <h4 style={mt20}>非受控组件：</h4>
+            <Corn
+                defaultValue={value3}
+                onChange={value => {
+                    setValue3(value);
+                }}
+            />
+            {value3}
+
+            <h4 style={mt20}>自定义表达式：</h4>
+            <Corn
+                value={value4}
+                onChange={value => {
+                    setValue4(value);
+                }}
+            />
+            {value4}
+        </Fragment>
+    );
+}
+```
---
+++ src/index.js
@@ -0,0 +1,300 @@
+import React, { Fragment, useState, useCallback, useRef, useEffect } from 'react';
+import { Select, TimePicker, Input } from 'antd';
+import moment from 'moment';
+
+const Option = Select.Option;
+const mwidth80 = { minWidth: 80, marginRight: 10 };
+const width150 = { width: 150 };
+const eachNum = (start, end, str) => {
+    const back = [];
+    for (let i = start; i <= end; i++) {
+        back.push({
+            value: `${i}`,
+            label: `${i}${str}`,
+        });
+    }
+    return back;
+};
+const dayOfWeekList = [
+    { label: '周一', value: '1' },
+    { label: '周二', value: '2' },
+    { label: '周三', value: '3' },
+    { label: '周四', value: '4' },
+    { label: '周五', value: '5' },
+    { label: '周六', value: '6' },
+    { label: '周日', value: '7' },
+];
+const minuteOfHoursList = eachNum(0, 59, '分');
+const dayOfMonthList = [
+    ...eachNum(1, 31, '号'),
+    {
+        label: '月末',
+        value: 'L',
+    },
+];
+const monthOfYearList = eachNum(1, 12, '月');
+const freqList = [
+    { value: 'everyHours', label: '每小时' },
+    { value: 'everyday', label: '每天' },
+    { value: 'week', label: '每周' },
+    { value: 'month', label: '每月' },
+    { value: 'year', label: '每年' },
+    { value: 'custom', label: '自定义' },
+];
+const cornFormat = (corn, mode) => {
+    const value = corn || '0 0 0 * * ? *';
+    const cronElements = value.split(' ');
+    let [ss, mm, HH, dd, MM, week, yyyy] = cronElements;
+    let freq;
+    if (yyyy !== '*' || /[-\/#]/.test(value) || (!mode && /[,]/.test(value))) {
+        freq = 'custom';
+    } else if (week !== '?') {
+        freq = 'week';
+    } else if (MM === '*' && dd === '*' && HH === '*' && ss === '*') {
+        freq = 'everyHours';
+    } else if (MM === '*' && dd === '*') {
+        freq = 'everyday';
+    } else if (MM === '*') {
+        freq = 'month';
+    } else if (MM !== '*') {
+        freq = 'year';
+    }
+
+    return {
+        freq,
+        stringValue: value,
+        ss: parseInt(ss) || 0,
+        mm: freq === 'everyHours' && !mode ? mm.split(',').filter(i => !!i) : parseInt(mm) || 0,
+        HH: parseInt(HH) || 0,
+        dd: dd.split(',').filter(i => !!i),
+        MM: MM.split(',').filter(i => !!i),
+        week: week.split(',').filter(i => !!i),
+        yyyy,
+    };
+};
+const cornStringify = ({ freq, stringValue, ss, mm, HH, dd, MM, week, yyyy }) => {
+    if (freq === 'custom') {
+        return stringValue;
+    } else if (freq === 'year') {
+        week = '?';
+        if (!dd || dd.length <= 0) {
+            dd = '*';
+        }
+    } else if (freq === 'month') {
+        MM = '*';
+        week = '?';
+    } else if (freq === 'week') {
+        MM = '*';
+        dd = '?';
+    } else if (freq === 'everyday') {
+        MM = '*';
+        week = '?';
+        dd = '*';
+    } else if (freq === 'everyHours') {
+        MM = '*';
+        week = '?';
+        dd = '*';
+        HH = '*';
+        ss = '*';
+    }
+
+    return `${ss} ${mm} ${HH} ${dd} ${MM} ${week} ${yyyy}`;
+};
+
+export default function CronForm({ defaultValue, value, onChange, multiple }) {
+    const [objValue, setObjValue] = useState({});
+    const thisCron = useRef('');
+    const changeValue = useCallback(newObj => {
+        const cronString = cornStringify(newObj);
+        thisCron.current = cronString;
+        onChange && onChange(cronString);
+    });
+    const onFreqChanged = useCallback(freq => {
+        setObjValue(oldObj => {
+            const newObj = {
+                ...oldObj,
+                freq,
+                week: freq === 'week' ? ['1'] : [],
+                dd: freq === 'month' ? ['1'] : [],
+                mm: Array.isArray(oldObj.mm) ? 0 : oldObj.mm,
+                ...(freq === 'everyHours' && multiple
+                    ? {
+                          mm: ['0'],
+                      }
+                    : {}),
+                MM: freq === 'year' ? ['1'] : '*',
+                ...(freq === 'custom'
+                    ? {
+                          stringValue: cornStringify(oldObj),
+                      }
+                    : {}),
+            };
+            changeValue(newObj);
+            return newObj;
+        });
+    }, []);
+    const onMonthOfYearChanged = useCallback(MM => {
+        setObjValue(oldObj => {
+            const newObj = {
+                ...oldObj,
+                MM,
+            };
+            changeValue(newObj);
+            return newObj;
+        });
+    }, []);
+    const onDayOfWeekChanged = useCallback(week => {
+        setObjValue(oldObj => {
+            const newObj = {
+                ...oldObj,
+                week,
+            };
+            changeValue(newObj);
+            return newObj;
+        });
+    }, []);
+    const onDayOfMonthChanged = useCallback(dd => {
+        setObjValue(oldObj => {
+            const newObj = {
+                ...oldObj,
+                dd,
+            };
+            changeValue(newObj);
+            return newObj;
+        });
+    }, []);
+    const onFreqTimeChanged = useCallback(time => {
+        setObjValue(oldObj => {
+            const newTime = time
+                ? { ss: time.second(), mm: time.minute(), HH: time.hour() }
+                : { ss: 0, mm: 0, HH: 0 };
+            const newObj = {
+                ...oldObj,
+                ...newTime,
+            };
+            changeValue(newObj);
+            return newObj;
+        });
+    }, []);
+    const onMinuteOfHoursListChanged = useCallback(mm => {
+        setObjValue(oldObj => {
+            const newObj = {
+                ...oldObj,
+                mm,
+            };
+            changeValue(newObj);
+            return newObj;
+        });
+    }, []);
+    const onStringValueChanged = useCallback(e => {
+        setObjValue(oldObj => {
+            const newObj = {
+                ...oldObj,
+                stringValue: e.target.value,
+            };
+            changeValue(newObj);
+            return newObj;
+        });
+    }, []);
+    useEffect(() => {
+        thisCron.current = value;
+        setObjValue(cornFormat(value || defaultValue, multiple));
+    }, []);
+    useEffect(() => {
+        if (thisCron.current !== value) {
+            thisCron.current = value;
+            setObjValue(cornFormat(value, multiple));
+        }
+    }, [value]);
+    const { freq, stringValue, ss, mm, HH, dd = [], MM, week = [], yyyy } = objValue;
+    const mode = multiple ? 'multiple' : undefined;
+    const isYear = freq === 'year',
+        isMonth = freq === 'month',
+        isWeek = freq === 'week',
+        isHours = freq === 'everyHours',
+        isCustom = freq === 'custom';
+    return (
+        <Fragment>
+            <Select value={freq} onChange={onFreqChanged} style={mwidth80}>
+                {freqList.map(({ value, label }) => (
+                    <Option key={value} value={value}>
+                        {label}
+                    </Option>
+                ))}
+            </Select>
+
+            {isYear && (
+                <Select
+                    value={MM}
+                    onChange={onMonthOfYearChanged}
+                    mode={mode}
+                    style={mwidth80}
+                    placeholder="月份"
+                >
+                    {monthOfYearList.map(({ value, label }) => (
+                        <Option key={value} value={value}>
+                            {label}
+                        </Option>
+                    ))}
+                </Select>
+            )}
+
+            {(isYear || isMonth) && (
+                <Select
+                    value={dd}
+                    onChange={onDayOfMonthChanged}
+                    mode={mode}
+                    style={mwidth80}
+                    placeholder="日期"
+                    allowClear={isYear}
+                >
+                    {dayOfMonthList.map(({ value, label }) => (
+                        <Option key={value} value={value}>
+                            {label}
+                        </Option>
+                    ))}
+                </Select>
+            )}
+
+            {isWeek && (
+                <Select
+                    value={week}
+                    onChange={onDayOfWeekChanged}
+                    mode={mode}
+                    style={mwidth80}
+                    placeholder="日期"
+                >
+                    {dayOfWeekList.map(({ value, label }) => (
+                        <Option key={value} value={value}>
+                            {label}
+                        </Option>
+                    ))}
+                </Select>
+            )}
+
+            {isHours && (
+                <Select
+                    value={mm}
+                    onChange={onMinuteOfHoursListChanged}
+                    mode={mode}
+                    style={mwidth80}
+                    placeholder="分钟"
+                >
+                    {minuteOfHoursList.map(({ value, label }) => (
+                        <Option key={value} value={value}>
+                            {label}
+                        </Option>
+                    ))}
+                </Select>
+            )}
+
+            {!isHours && !isCustom && (
+                <TimePicker value={moment({ h: HH, m: mm, s: ss })} onChange={onFreqTimeChanged} />
+            )}
+
+            {isCustom && (
+                <Input style={width150} value={stringValue} onChange={onStringValueChanged} />
+            )}
+        </Fragment>
+    );
+}
