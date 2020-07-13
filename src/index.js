dbfe368d19ad90e239a4734842e2fc87711bbecc
wangxiaofeid <979436452@qq.com>
2020-07-13 16:31:13 +0800

    dev

--- src/index.js
+++ src/index.js
@@ -16,13 +16,13 @@ const eachNum = (start, end, str) => {
     return back;
 };
 const dayOfWeekList = [
-    { label: '周一', value: '1' },
-    { label: '周二', value: '2' },
-    { label: '周三', value: '3' },
-    { label: '周四', value: '4' },
-    { label: '周五', value: '5' },
-    { label: '周六', value: '6' },
-    { label: '周日', value: '7' },
+    { label: '周一', value: '2' },
+    { label: '周二', value: '3' },
+    { label: '周三', value: '4' },
+    { label: '周四', value: '5' },
+    { label: '周五', value: '6' },
+    { label: '周六', value: '7' },
+    { label: '周日', value: '1' },
 ];
 const minuteOfHoursList = eachNum(0, 59, '分');
 const dayOfMonthList = [
